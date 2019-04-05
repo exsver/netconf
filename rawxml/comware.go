fc67e352d9d6d316260b8b77928f6ab056ffa5bf
Vadym Khomenko <exsver@gmail.com>
2019-04-05 16:19:04 +0300

    add more raw xml

--- rawxml/comware.go
+++ rawxml/comware.go
@@ -1,7 +1,69 @@
 package rawxml
 
 var XMLMessagesHPE = map[string]string{
-	"GetDevice": `<rpc message-id="101" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+	"GetSubtree": `<rpc message-id="101" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                              <get>
+                                <filter type="subtree">
+                                  <top xmlns="http://www.hp.com/netconf/data:1.0">
+                                    <ACL/>
+                                  </top>
+                                </filter>
+                              </get>
+                            </rpc>`,
+	/* Subtree list:
+		<ACL/>                           5130 + |
+		<ARP/>                           5130 + |
+		<ATK/>							 5130 - |
+	    <CAR/>
+	    <Configuration/>                 5130 + |
+		<Device/>
+	    <Device><ExtPhysicalEntities><Entity><PhysicalIndex>228</PhysicalIndex><AdminState/><OperState/><StandbyState/><CpuUsage/><CpuUsageThreshold/><MemUsage/><MemAvgUsage/><MemSize/><PhyMemSize/></Entity></ExtPhysicalEntities></Device>
+	    <Device><Boards><Board/></Boards><PhysicalEntities><Entity><PhysicalIndex/><Chassis/><Slot/><SubSlot/><Description/><VendorType/><ContainedIn/><Class/><ParentRelPos/><Name/></Entity></PhysicalEntities><ExtPhysicalEntities><Entity><PhysicalIndex/><AdminState/><OperState/><StandbyState/><CpuUsage/><CpuUsageThreshold/><MemUsage/><MemAvgUsage/><MemSize/><PhyMemSize/></Entity></ExtPhysicalEntities></Device><Ifmgr><Interfaces><Interface><IfIndex/><Name/><AbbreviatedName/><PortIndex/><PhysicalIndex/><ifTypeExt/></Interface></Interfaces><EthInterfaces><Interface><IfIndex/><Combo/></Interface></EthInterfaces><EthInterfaceCapabilities><Interface><IfIndex/><Combo/></Interface></EthInterfaceCapabilities></Ifmgr>
+	    <DHCP/>
+		<DHCPSP/>
+		<DHCPSP6/>
+		<Domain/>                        5130 + |
+	    <FileSystem/>                    5130 + |
+	    <FTP/>
+	    <Fundamentals/>
+	    <HardwareQueue/>
+		<Ifmgr/>                         5130 + |
+		<IPCIM/>
+		<IPV4ADDRESS/>                   5130 + |
+	    <IRF/>							 5130 + | 5940 +
+		<L2VPN/>								| 5940 +
+		<LAGG/>                          5130 + |
+		<Login/>
+	    <License/>                       5130 - |
+		<MAC/>							 5130 + | 5940 +
+	    <MGROUP/>                        5130 + |
+	    <MLDSnooping/>
+	    <MQC/>
+		<NTP/>                           5130 + |
+	    <ND/>
+	    <Package/>
+	    <PasswordControl/>
+	    <PBR/>                           5130 + |
+	    <PIM/>
+	    <Radius/>                        5130 + |
+		<RBAC/>                          5130 + |
+	    <ResourceMonitor/>               5130 - | 5940 +
+	    <RMON/>
+	    <Route/>
+	    <SFLOW/>
+	    <Super/>                         5130 - |
+		<SNMP/>                          5130 + |
+		<SSH/>                           5130 + |
+		<StaticRoute/>                   5130 + |
+		<STP/>                           5130 + |
+		<Syslog/>
+	    <URPF4/>
+		<UserAccounts/>                  5130 + |
+		<VCF/>                           5130 + |
+		<VLAN/>                          5130 + |
+		<VXLAN/>								| 5940 +
+	*/
+	"GetDevice": `<rpc message-id="102" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
                    <get>
                      <filter type="subtree">
                        <top xmlns="http://www.hp.com/netconf/data:1.0">
@@ -10,7 +72,7 @@ var XMLMessagesHPE = map[string]string{
                      </filter>
                    </get>
                  </rpc>`,
-	"GetDeviceBaseHostname": `<rpc message-id="102" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+	"GetDeviceBaseHostname": `<rpc message-id="103" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
                                 <get-config>
                                   <source>
                                     <running/>
@@ -26,7 +88,95 @@ var XMLMessagesHPE = map[string]string{
                                 </filter>
                               </get-config>
                              </rpc> `,
-	"SaveForce": `<rpc message-id="103" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0"><save/></rpc>`,
+	"GetTransceivers": `<rpc message-id="104" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                          <get>
+                            <filter type="subtree">
+                              <top xmlns="http://www.hp.com/netconf/data:1.0">
+                                <Device>
+                                  <Transceivers/>
+                                </Device>
+                              </top>
+                            </filter>
+                          </get>
+                        </rpc> `,
+	"GetSessions": `<rpc message-id="105" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0"><get-sessions/></rpc>`,
+	"GetRunningConfig": `<rpc xmlns="urn:ietf:params:xml:ns:netconf:base:1.0" message-id="103">
+                           <get-config>
+                             <source>
+                               <running/>
+                             </source>
+                           </get-config>
+                         </rpc>`,
+	//https://support.hpe.com/hpsc/doc/public/display?docId=emr_na-c04966589
+	"LockRunningConfig": `<rpc message-id="301" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                            <lock>
+                              <target>
+                                <running/>
+                              </target>
+                            </lock>
+                          </rpc>`,
+	"UnLockRunningConfig": `<rpc message-id="302" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                              <unlock>
+                                <target>
+                                  <running/>
+                                </target>
+                              </unlock>
+                            </rpc>`,
+	"SaveConfig": `<rpc message-id="303" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                     <save>
+                       <file>my_config.cfg</file>
+                     </save>
+                   </rpc>`,
+	"SaveForce": `<rpc message-id="304" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0"><save/></rpc>`,
+	"GetIfConfig": `<rpc message-id="104" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                      <get-config>
+                        <source>
+                          <running/>
+                        </source>
+                        <filter type="subtree">
+                          <top xmlns="http://www.hp.com/netconf/config:1.0">
+                            <Ifmgr>
+                              <Interfaces/>
+                            </Ifmgr>
+                          </top>
+                        </filter>
+                      </get-config>
+                    </rpc> `,
+	"GetIf": `<rpc message-id="111" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                <get>
+                  <filter type="subtree">
+                    <top xmlns="http://www.hp.com/netconf/data:1.0">
+                      <Ifmgr>
+                        <Interfaces/>
+                      </Ifmgr>
+                    </top>
+                  </filter>
+                </get>
+              </rpc> `,
+	"GetEthIfConfig": `<rpc message-id="105" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                         <get-config>
+                           <source>
+                             <running/>
+                           </source>
+                           <filter type="subtree">
+                             <top xmlns="http://www.hp.com/netconf/config:1.0">
+                               <Ifmgr>
+                                 <EthInterfaces/>
+                               </Ifmgr>
+                             </top>
+                           </filter>
+                         </get-config>
+                       </rpc> `,
+	"GetIfConfigFilter": `<rpc message-id="105" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                            <get>
+                              <filter type="subtree">
+                                <top xmlns="http://www.hp.com/netconf/data:1.0">
+                                  <Ifmgr>
+                                  </Ifmgr>
+                                </top>
+                              </filter>
+                            </get>
+                          </rpc> `,
 	"GetEthIf": `<rpc message-id="111" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
                    <get>
                      <filter type="subtree">
@@ -71,4 +221,170 @@ var XMLMessagesHPE = map[string]string{
                                  </filter>
                                </get>
                              </rpc>`,
+	"GetBindings": `<rpc message-id="106" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                      <get-config>
+                        <source>
+                          <running/>
+                        </source>
+                        <filter type="subtree">
+                          <top xmlns="http://www.hp.com/netconf/config:1.0">
+                            <IPCIM>
+                              <IpSourceBindingInterface/>
+                            </IPCIM>
+                          </top>
+                        </filter>
+                      </get-config>
+                    </rpc> `,
+	"GetARPDetection": `<rpc message-id="107" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                          <get-config>
+                            <source>
+                              <running/>
+                            </source>
+                            <filter type="subtree">
+                              <top xmlns="http://www.hp.com/netconf/config:1.0">
+                                <ARP/>
+                              </top>
+                            </filter>
+                          </get-config>
+                        </rpc> `,
+	"GetARPTable": `<rpc message-id="108" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                      <get>
+                        <filter type="subtree">
+                          <top xmlns="http://www.hp.com/netconf/config:1.0">
+                            <ARP/>
+                          </top>
+                        </filter>
+                      </get>
+                    </rpc> `,
+	"GetMACTable": `<rpc message-id="111" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                      <get>
+                        <filter type="subtree">
+                          <top xmlns="http://www.hp.com/netconf/data:1.0">
+                            <MAC>
+                              <MacUnicastTable/>
+                            </MAC>
+                          </top>
+                        </filter>
+                      </get>
+                    </rpc>`,
+	"CLI": `<rpc message-id="109" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+              <CLI>
+                <Execution>
+                  display vlan
+                </Execution>
+              </CLI>
+            </rpc> `,
+	"SetLogBuffer": `<rpc message-id="200" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                       <edit-config>
+                         <target>
+                           <running/>
+                         </target>
+                         <config>
+                           <top xmlns="http://www.hp.com/netconf/config:1.0" xmlns:name="urn:ietf:params:xml:ns:netconf:base:1.0" name:operation="merge">
+                             <Syslog>
+                               <LogBuffer>
+                                 <BufferSize>512</BufferSize>
+                               </LogBuffer>
+                             </Syslog>
+                           </top>
+                         </config>
+	                   </edit-config>
+	</rpc>`,
+	"SetBinding": `<rpc message-id="201" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                     <edit-config>
+                       <target>
+                         <running/>
+                       </target>
+                       <config>
+                         <top xmlns="http://www.hp.com/netconf/config:1.0" xmlns:name="urn:ietf:params:xml:ns:netconf:base:1.0" name:operation="merge">
+                           <IPCIM>
+                             <IpSourceBindingInterface>
+                               <SourceBinding>
+                                 <IfIndex>8</IfIndex>
+                                 <Ipv4Address>10.10.0.10</Ipv4Address>
+                                 <MacAddress>52-54-00-9A-C9-20</MacAddress>
+                                 <VLANID>220</VLANID>
+                               </SourceBinding>
+                             </IpSourceBindingInterface>
+                           </IPCIM>
+                         </top>
+                       </config>
+	                 </edit-config>
+                   </rpc>`,
+	"Get-bulk": `<rpc message-id="101" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0" xmlns:web="urn:ietf:params:xml:ns:netconf:base:1.0">
+                  <get-bulk>
+                    <filter type="subtree">
+                      <top xmlns="http://www.hp.com/netconf/data:1.0" xmlns:web="http://www.hp.com/netconf/base:1.0">
+                        <Ifmgr>
+                          <Interfaces web:count="5"></Interfaces>
+                        </Ifmgr>
+                      </top>
+                    </filter>
+                  </get-bulk>
+                 </rpc>`,
+	"Get-mon": `<rpc message-id='101' xmlns='urn:ietf:params:xml:ns:netconf:base:1.0' xmlns:web='urn:ietf:params:xml:ns:netconf:base:1.0'>
+                  <get>
+                    <filter type='subtree'>
+                      <top xmlns='http://www.hp.com/netconf/data:1.0' xmlns:web='http://www.hp.com/netconf/base:1.0' xmlns:data='http://www.hp.com/netconf/data:1.0' web:count='1'>
+                        <Device>
+                          <ExtPhysicalEntities>
+                            <Entity>
+                              <PhysicalIndex>192</PhysicalIndex>
+                              <AdminState/>
+                              <OperState/>
+                              <StandbyState/>
+                              <CpuUsage/>
+                              <CpuUsageThreshold/>
+                              <MemUsage/>
+                              <MemAvgUsage/>
+                              <MemSize/>
+                              <PhyMemSize/>
+                            </Entity>
+                          </ExtPhysicalEntities>
+                        </Device>
+                      </top>
+                    </filter>
+                  </get>
+                </rpc>`,
+	"regexp": `<rpc xmlns="urn:ietf:params:xml:ns:netconf:base:1.0" message-id="1">
+      <get>
+        <filter type="subtree">
+            <top xmlns="http://www.hp.com/netconf/data:1.0">
+                <Ifmgr>
+                    <Ports>
+                        <Port>
+							<IfIndex xmlns:re="http://www.hp.com/netconf/base:1.0" re:regExp="2"/>
+                        </Port>
+                    </Ports>
+                </Ifmgr>
+            </top>
+        </filter>
+      </get>
+    </rpc>`,
+	"yang": `<rpc message-id="101" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+			<get-schema xmlns="urn:ietf:params:xml:ns:yang:ietf-netconf-monitoring">
+				<identifier>ietf-netconf</identifier>
+				<version>2014-10-12</version>
+				<format>yang</format>
+				</get-schema>
+			</rpc>`,
+	"ConditionalMatch": `<rpc xmlns="urn:ietf:params:xml:ns:netconf:base:1.0" message-id="1">
+<get>
+	<filter type="subtree">
+		<top xmlns="http://www.hp.com/netconf/data:1.0">
+			<MAC>
+				<MacUnicastTable>
+					<Unicast>
+						<VLANID>99</VLANID>
+						<MacAddress/>
+						<PortIndex xmlns:re="http://www.hp.com/netconf/base:1.0" re:match="notEqual:719"/>
+						<Status/>
+						<Aging/>
+					</Unicast>
+				</MacUnicastTable>
+			</MAC>
+		</top>
+	</filter>
+</get>
+</rpc>`,
 }
--- rawxml/junos.go
+++ rawxml/junos.go
@@ -1,6 +1,8 @@
 package rawxml
 
 var XMLMessagesJunOS = map[string]string{
+	"Get":                      `<rpc><get/></rpc>`,
+	"CLI":                      `<rpc><command format=\"text\">show configuration vlans</command></rpc>`,
 	"GetChassisInventory":      `<rpc><get-chassis-inventory/></rpc>`,
 	"GetSoftwareInformation":   `<rpc><get-software-information/></rpc>`,
 	"GetSystemInformation":     `<rpc><get-system-information/></rpc>`,
@@ -10,4 +12,169 @@ var XMLMessagesJunOS = map[string]string{
 	"GetARP":                   `<rpc><get-arp-table-information/></rpc>`,
 	"Commit":                   `<rpc><commit/></rpc>`,
 	"Reboot":                   `<rpc><request-reboot/></rpc>`,
+	"GetRunningConfig": `<rpc>
+                            <get-config>
+                              <source>
+                                <running/>
+                              </source>
+                            </get-config>
+                           </rpc>`,
+	"GetRunningConfigFilter": `<rpc>
+                                <get-config>
+                                  <source>
+                                    <running/>
+                                  </source>
+                                  <filter type="subtree">
+                                    <configuration>
+                                      <system/>
+                                    </configuration>
+                                  </filter>
+                                </get-config>
+                               </rpc>`,
+	"LockConfig": `<rpc>
+                     <lock>
+                       <target>
+                         <candidate/>
+                       </target>
+                     </lock>
+                   </rpc>`,
+	"UnLockConfig": `<rpc>
+                      <unlock>
+                        <target>
+                          <candidate/>
+                        </target>
+                      </unlock>
+                     </rpc>`,
+	"SetHostname": `<rpc>
+                      <edit-config>
+                        <target>
+                          <candidate/>
+                        </target>
+                        <config>
+                          <configuration>
+                            <system>
+                              <host-name>test</host-name>
+                            </system>
+	                      </configuration>
+                        </config>
+                      </edit-config>
+                     </rpc>`,
+	"CommitCheck": `<rpc>
+                      <validate>
+                        <source>
+                          <candidate/>
+                        </source>
+                      </validate>
+                     </rpc>`,
+	"CommitConfirmed": `<rpc>
+                           <commit>
+                             <confirmed/>
+                             <confirm-timeout>" 60 "</confirm-timeout>
+                           </commit>
+                         </rpc>`, //time in seconds
+	"RollbackCompare": `<rpc>
+                          <get-rollback-information>
+                            <rollback>0</rollback>
+                            <compare>1</compare>
+                          </get-rollback-information>
+                        </rpc>`,
+	"LoadConfigurationRolback": `<rpc><load-configuration rollback="0"/></rpc>`,
+	"GetConfig": `<rpc>
+                   <get-config>
+                     <source>
+                       <candidate/>
+                     </source>
+                   </get-config>
+                 </rpc>`,
+	"GetConfigSubtree": `<rpc>
+                          <get-config>
+                            <source>
+                              <candidate/>
+                            </source>
+                            <filter type="subtree">
+                              <configuration>
+                                <system>
+                                  <login/>
+                                </system>
+                              </configuration>
+                            </filter>
+                          </get-config>
+                         </rpc>`,
+	"GetConfiguration": `<rpc>
+                   <get-configuration format="set" inherit="defaults">
+                    </get-configuration>
+                 </rpc>`, //format="( json | set | text | xml )"
+	"LoadConfigurationText": `<rpc>
+                           <load-configuration action="merge" format="text">
+                             <configuration-text>
+vlans {
+    v101 {
+        description test-vlan101;
+        vlan-id 101;
+    }
+}
+                             </configuration-text>
+                           </load-configuration></rpc>`,
+	"LoadConfigurationXML":    `<rpc></rpc>`,
+	"GetInterfaceInformation": `<rpc><get-interface-information/></rpc>`,
+	"GetInterfaceDescription": `<rpc>
+                                  <get-interface-information>
+	                                <descriptions/>
+	                              </get-interface-information>
+	                            </rpc>`,
+	"GetInterfaceInformationText": `<rpc><get-interface-information format="text"></get-interface-information></rpc>`,
+	"GetInterfaceInformationFilter": `<rpc>
+                                        <get-interface-information format="xml">
+                                           <interface-name>em0</interface-name>
+                                        </get-interface-information>
+                                      </rpc>`, //format="(ascii | json| text | xml)"
+	"GetInterfaceInformationDetail": `<rpc>
+                                        <get-interface-information>
+                                          <detail/>
+                                        </get-interface-information>
+                                      </rpc>`,
+	"GetInterfaceInformationTerse": `<rpc>
+                                       <get-interface-information>
+                                         <terse/>
+                                       </get-interface-information>
+                                     </rpc>`,
+	"GetInterfaceBrief": `<rpc>
+                             <get-interface-information>
+                               <brief/>
+                               <interface-name>et-0/0/0</interface-name>
+                             </get-interface-information>
+                           </rpc>`,
+	"GetInterfaceOptics": `<rpc>
+                             <get-interface-optics-diagnostics-information>
+                               <interface-name>et-0/0/0</interface-name>
+                             </get-interface-optics-diagnostics-information>
+                           </rpc>`,
+	"Ping": `<rpc>
+               <ping>
+                 <host>8.8.8.8</host>
+                 <count>1</count>
+               </ping>
+              </rpc>`,
+	"GetXNM": `<rpc>
+                 <get-xnm-information>
+                   <type>xml-schema</type>
+                   <namespace>junos-configuration</namespace>
+                 </get-xnm-information>
+               </rpc>`,
+	"AddVlan": `<rpc xmlns="urn:ietf:params:xml:ns:netconf:base:1.0" message-id="2">
+<edit-config>
+	<target><candidate/></target>
+	<default-operation>merge</default-operation>
+	<config>
+		<configuration>
+			<vlans>
+				<vlan>
+					<name>add-test-vlan</name>
+					<vlan-id>36</vlan-id>
+				</vlan>
+			</vlans>
+		</configuration>
+	</config>
+</edit-config>
+</rpc>`,
 }
