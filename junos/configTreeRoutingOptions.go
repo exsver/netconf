186573a4e6ef2861fd8257a3924f19551c15e64e
Vadym Khomenko <exsver@gmail.com>
2020-03-05 11:05:09 +0200

    junos: add StaticRoute.ConvertToConfig()

--- junos/configTreeRoutingOptions.go
+++ junos/configTreeRoutingOptions.go
@@ -8,12 +8,41 @@ type RoutingOptions struct {
 }
 
 type RoutingOptionsStatic struct {
-	XMLName xml.Name `xml:"static"`
-	Routes  []Route  `xml:"route"`
+	XMLName xml.Name      `xml:"static"`
+	Routes  []StaticRoute `xml:"route"`
 }
 
-type Route struct {
-	XMLName xml.Name `xml:"route"`
-	Name    string   `xml:"name"`
-	NextHop string   `xml:"next-hop"`
+type StaticRoute struct {
+	XMLName                xml.Name `xml:"route"`
+	NetconfConfigOperation string   `xml:"operation,attr,omitempty"`
+	Name                   string   `xml:"name"`
+	NextTable              string   `xml:"next-table,omitempty"`
+	NextHop                []string `xml:"next-hop"`
+	Active                 bool     `xml:"active,omitempty"`
+	Passive                bool     `xml:"passive,omitempty"`
+	Discard                bool     `xml:"discard,omitempty"`
+	Readvertise            bool     `xml:"readvertise,omitempty"`
+	NoReadvertise          bool     `xml:"no-readvertise,omitempty"`
+	Receive                bool     `xml:"receive,omitempty"`
+	Reject                 bool     `xml:"reject,omitempty"`
+	Resolve                bool     `xml:"resolve,omitempty"`
+	NoResolve              bool     `xml:"no-resolve,omitempty"`
+	Retain                 bool     `xml:"retain,omitempty"`
+	NoRetain               bool     `xml:"no-retain,omitempty"`
+	Install                bool     `xml:"install,omitempty"`
+	NoInstall              bool     `xml:"no-install,omitempty"`
+	LongestMatch           bool     `xml:"longest-match,omitempty"`
+	NoLongestMatch         bool     `xml:"no-longest-match,omitempty"`
+}
+
+func (staticRoute *StaticRoute) ConvertToConfig() *Config {
+	return &Config{
+		Configuration: &Configuration{
+			RoutingOptions: &RoutingOptions{
+				Static: &RoutingOptionsStatic{
+					Routes:  []StaticRoute{*staticRoute},
+				},
+			},
+		},
+	}
 }
