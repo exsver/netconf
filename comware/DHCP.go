e5d2ec1e50aa7e80a863bb5e32a66b3f6ed85b85
Vadym Khomenko <exsver@gmail.com>
2023-02-16 12:08:12 +0200

    fix DHCP data

--- comware/DHCP.go
+++ comware/DHCP.go
@@ -2,7 +2,7 @@ package comware
 
 import "github.com/exsver/netconf/netconf"
 
-func (targetDevice *TargetDevice) GetDataDHCPServer() (*DHCP, error) {
+func (targetDevice *TargetDevice) GetDataDHCP() (*DHCP, error) {
 	request := netconf.RPCMessage{
 		InnerXML: []byte(`<get><filter type="subtree"><top xmlns="http://www.hp.com/netconf/data:1.0"><DHCP/></top></filter></get>`),
 		Xmlns:    []string{netconf.BaseURI},
@@ -14,4 +14,32 @@ func (targetDevice *TargetDevice) GetDataDHCPServer() (*DHCP, error) {
 	}
 
 	return data.Top.DHCP, nil
+}
+
+func (targetDevice *TargetDevice) GetDataDHCPServerIpPools() (*DHCPServerIPPool, error) {
+	request := netconf.RPCMessage{
+		InnerXML: []byte(`<get><filter type="subtree"><top xmlns="http://www.hp.com/netconf/data:1.0"><DHCP><DHCPServerIpPool/></DHCP></top></filter></get>`),
+		Xmlns:    []string{netconf.BaseURI},
+	}
+
+	data, err := targetDevice.RetrieveData(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return data.Top.DHCP.DHCPServerIPPool, nil
+}
+
+func (targetDevice *TargetDevice) GetDataDHCPServerIpInUse() (*DHCPServerIPInUse, error) {
+	request := netconf.RPCMessage{
+		InnerXML: []byte(`<get><filter type="subtree"><top xmlns="http://www.hp.com/netconf/data:1.0"><DHCP><DHCPServerIpInUse/></DHCP></top></filter></get>`),
+		Xmlns:    []string{netconf.BaseURI},
+	}
+
+	data, err := targetDevice.RetrieveData(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return data.Top.DHCP.DHCPServerIPInUse, nil
 }
